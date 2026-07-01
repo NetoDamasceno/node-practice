750362c63ba344c2b9c4382e2b1f463c3d27f061
NetoDamasceno <luiznetto13@gmail.com>
2026-07-01 16:45:58 -0300

    feat: complete crud with express, mongoose and ejs

---
+++ src/database/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectToDataBase = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@users.zyjmo.mongodb.net/database?appName=Users`,
+    );
+    console.log("Conexão ao banco de dados realizada com sucesso!");
+  } catch (error) {
+    console.error("Erro ao conectar ao MongoDB: ", error);
+  }
+};
+
+module.exports = connectToDataBase;
